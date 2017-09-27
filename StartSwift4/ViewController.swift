8cb52b7632af0672b8c9ec625fdb418df4d1b1ec
Vansa Pha <vs.lov.rs@gmail.com>
2017-09-27 14:13:58 +0700

    Initial Commit

---
+++ StartSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StartSwift4
+//
+//  Created by Vansa Pha on 9/27/17.
+//  Copyright © 2017 Vansa Pha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
