6daba1f952f71cbeafcbda1009d8827045e0b69b
Vansa Pha <vs.lov.rs@gmail.com>
2017-09-27 17:30:43 +0700

    animation screen

--- StartSwift4/ViewController.swift
+++ StartSwift4/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //visual connection
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var liiv: UILabel!
+    @IBOutlet weak var kbBank: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func powerAction(_ sender: UIButton) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerBtn.isHidden = true
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 160, width: 375, height: 402)
+        }) { (finished) in
+            self.liiv.isHidden = false
+            self.kbBank.isHidden = false
+        }
     }
-
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 }
 
