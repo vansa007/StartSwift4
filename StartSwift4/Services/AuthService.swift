66c09e8426b6ec44de1fe704ef7e90109cdeacf4
Vansa Pha <vs.lov.rs@gmail.com>
2017-10-03 17:32:08 +0700

    Auth Service

--- StartSwift4/Services/AuthService.swift
+++ StartSwift4/Services/AuthService.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import Alamofire
+
+class AuthService {
+    static let instance = AuthService()
+    let defaults = UserDefaults.standard
+    var isLoggedIn: Bool {
+        get { return defaults.bool(forKey: LOGGED_IN_KEY) }
+        set { defaults.set(newValue, forKey: LOGGED_IN_KEY) }
+    }
+    var authToken: String {
+        get { return defaults.value(forKey: TOKEN_KEY) as! String }
+        set { defaults.set(newValue, forKey: TOKEN_KEY) }
+    }
+    var userEmail: String {
+        get { return defaults.value(forKey: USER_EMAIL) as! String }
+        set { defaults.set(newValue, forKey: USER_EMAIL) }
+    }
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        //TODO: tomorrow video 063, 20:54
+    }
+}
---
+++ StartSwift4/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  StartSwift4
+//
+//  Created by Vansa Pha on 10/3/17.
+//  Copyright © 2017 Vansa Pha. All rights reserved.
+//
+
+import Foundation
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//Authentication
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
